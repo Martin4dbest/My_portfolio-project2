2e6434c047dc401edcd8b285674a6151c95f190b
Martin Agoha <you@martin4dtruth@gmail.com>
2023-09-10 13:58:41 +0100

    add

--- my_python_mini-project/quiz_game.py
+++ my_python_mini-project/quiz_game.py
@@ -38,3 +38,5 @@ else:
 
 print("You got " + str(score) + " questions correct!")
 print("You got " + str((score / 4) * 100) + "%.")
+
+
--- my_school-app/app.py
+++ my_school-app/app.py
@@ -1,8 +1,36 @@
-# Import necessary modules
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired
+from flask_wtf import FlaskForm
 
-# Create a Flask application
 app = Flask(__name__)
+
+# Configure the database (SQLite in this example)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'
+db = SQLAlchemy(app)
+
+
+# Define the News model
+
+
+class News(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    pub_date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __init__(self, title, content):
+        self.title = title
+        self.content = content
+
+
+class NewsForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Add News')
+
 
 # Define routes for your website
 @app.route('/')
@@ -13,7 +41,30 @@ def home():
 def about():
     return render_template('about.html')
 
+@app.route('/news')
+def news():
+    news_items = News.query.all()
+    return render_template('news.html', news_items=news_items)
+
+@app.route('/add_news', methods=['GET', 'POST'])
+def add_news():
+    form = NewsForm()
+
+    if form.validate_on_submit():
+        title = form.title.data
+        content = form.content.data
+
+        if title and content:
+            new_news = News(title=title, content=content)
+            db.session.add(new_news)
+            db.session.commit()
+            flash('News added successfully!', 'success')
+            return redirect('/news')
+    return render_template('add_news.html', form=form)
 # You can add more routes for different pages
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
